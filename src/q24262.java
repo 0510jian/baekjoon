beab4e5072b70eb4ceaf4cd8fa804416e88f3019
Jian <124128448+0510jian@users.noreply.github.com>
2023-05-10 22:48:34 +0900

    ct: bj 24262,24263,24265,24265 upload

---
+++ src/q24262.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class q24262 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("1\n0");
+    }
+}
