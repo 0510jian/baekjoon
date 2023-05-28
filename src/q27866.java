f5015af1378c0873ee43d6b192c946f1a8d74478
Jian <124128448+0510jian@users.noreply.github.com>
2023-05-28 23:16:38 +0900

    ct: bj 27866 upload

---
+++ src/q27866.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class q27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] S = sc.nextLine().split("");
+        int i = sc.nextInt();
+
+        System.out.print(S[i-1]);
+    }
+}
