1d267c012b3a9d09d1f86e32ab6087f072c079da
Jian <124128448+0510jian@users.noreply.github.com>
2023-08-18 11:10:58 +0900

    ct: bj 28446 upload

---
+++ src/q28446.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class q28446 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int M = Integer.parseInt(br.readLine());
+
+        HashMap<Integer, Integer> locker = new HashMap<>();
+        for(int i=0; i<M; i++) {
+            StringTokenizer tk = new StringTokenizer(br.readLine(), " ");
+            int command = Integer.parseInt(tk.nextToken());
+
+            if(command == 1) {
+                int index = Integer.parseInt(tk.nextToken());
+                int w = Integer.parseInt(tk.nextToken());
+                locker.put(w, index);
+            } else {
+                int w = Integer.parseInt(tk.nextToken());
+                bw.write(locker.get(w) + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
